5186bb88d9faf2fd7d530a572dc75113ee2040e6
kazunori.kimura <kazunori.kimura.js@gmail.com>
2018-10-22 14:58:11 +0900

    [add] convert lang files

--- format-keycode/convert.js
+++ format-keycode/convert.js
@@ -5,8 +5,91 @@ const HTML_FILE_BASE = '../downloads/So-net-all';
 
 const re = /getresult\(([0-9,]+)\)/ig;
 
-async function convert(filename) {
-  const data = await fs.readFile(path.resolve(HTML_FILE_BASE, filename), 'utf-8');
+const files = [
+  {
+    file: 'Myanmar_Pickup.html',
+    json: 'myanmar.json',
+  },
+  {
+    file: 'Myanmar_Pickup_2.html',
+    json: 'myanmar_basic.json',
+  },
+  {
+    file: 'Pashto_Pickup.html',
+    json: 'pashto.json',
+  },
+  {
+    file: 'Assamese_Pickup.html',
+    json: 'assamese.json',
+  },
+  {
+    file: 'Sanskrit_Pickup.html',
+    json: 'sanskrit.json',
+  },
+  {
+    file: 'Bengali_Pickup.html',
+    json: 'bengali.json',
+  },
+  {
+    file: 'kmer_Cambodian.html',
+    json: 'kmer.json',
+  },
+  {
+    file: 'Tamil_Pickup.html',
+    json: 'tamil.json',
+  },
+  {
+    file: 'Sinhala_EM_Pickup.html',
+    json: 'sinhala.json',
+  },
+  {
+    file: 'Napali_Pickup.html',
+    json: 'napali.json',
+  },
+  {
+    file: 'Hindi_Pickup.html',
+    json: 'hindi.json',
+  },
+  {
+    file: 'Syrian_Pickup.html',
+    json: 'syrian.json',
+  },
+  {
+    file: 'Turkish_Pickup.html',
+    json: 'turkish.json',
+  },
+  {
+    file: 'Turkish_Pho_Pickup.html',
+    json: 'turkish_pho.json',
+  },
+  {
+    file: 'Hangary_Pickup.html',
+    json: 'hangary.json',
+  },
+  {
+    file: 'English_Pho_Pickup.html',
+    json: 'english_pho.json',
+  },
+  {
+    file: 'Cyrillic_Pickup.html',
+    json: 'cyrillic.json',
+  },
+  {
+    file: 'Russian_Pickup.html',
+    json: 'russian.json',
+  },
+  {
+    file: 'Suomi_Pickup.html',
+    json: 'suomi.json',
+  },
+  {
+    file: 'French_Pho_Pickup.html',
+    json: 'french_pho.json',
+  },
+];
+
+async function convert({ file, json }) {
+  const data = await fs.readFile(path.resolve(HTML_FILE_BASE, file), 'utf-8');
   
   let matches;
   const lines = [];
@@ -18,21 +101,12 @@ async function convert(filename) {
       lines.push(`{ "code": [${item}] }`);
     }
   }
-  const json = `[${lines.join(',')}]`;
-  const obj = JSON.parse(json);
-  const file = filename.replace('.html', '.json');
-  await fs.writeJson(file, obj);
+  const jsonStr = `[\n  ${lines.join(',\n  ')}\n]`;
+  // const obj = JSON.parse(jsonStr);
+  await fs.writeFile(path.resolve('./lang', json), jsonStr);
 }
 
 async function main() {
-  const files = [
-    'Thai_Pho_Pickup.html',
-    'Pinyin_Pickup.html',
-    'Vietnam_Pickup.html',
-    'Lao_Pickup.html',
-    'Tibetan_Pickup.html',
-  ];
-
   const tasks = files.map(convert);
   await Promise.all(tasks);
 }
--- react-keyboard/README.md
+++ react-keyboard/README.md
@@ -5,11 +5,9 @@
 - アラビア語は右から左になるが、カーソル位置の独自実装が必要。後回し。
 - ヘブライ語は右から左になるが、カーソル位置の独自実装が必要。後回し。
 - シリア語、フォント指定ができていない。
-- トルコ語発音記号 ほぼボタンが '&#32;' となっている。 該当ボタンを削除して実装。
+- 文字コード指定が `0,0,0,0` となっているものは `0,0,0,32` (space) に置き換えて実装
 
-/Users/kimurakazunori/Repository/input-clue/downloads/So-net-all
-
-## 対応済み
+## ファイル一覧
 
 - [x] Thai_Pickup.html ＜タイ語＞
 - [x] Thai_Pho_Pickup.html ＜タイ語&amp;発音記号＞
@@ -17,17 +15,14 @@
 - [x] Vietnam_Pickup.html ＜ベトナム語・クォック・グー＞
 - [x] Lao_Pickup.html ＜ラオス語＞
 - [x] Tibetan_Pickup.html ＜チベット語＞
-
-## 未着手
-
-- [ ] Myanmar_Pickup.html ＜ミャンマー語＞
-- [ ] Myanmar_Pickup_2.html ＜ミャンマー語・標準＞
-- [ ] Pashto_Pickup.html ＜パシュトゥ語（アフガニスタン）＞
-- [ ] Assamese_Pickup.html ＜アッサム語＞
-- [ ] Sanskrit_Pickup.html ＜サンスクリット語＞
-- [ ] Bengali_Pickup.html ＜ベンガル語＞
-- [ ] kmer_Cambodian.html ＜クメール・カンボジア語＞
-- [ ] Tamil_Pickup.html ＜タミル語＞
+- [x] Myanmar_Pickup.html ＜ミャンマー語＞
+- [x] Myanmar_Pickup_2.html ＜ミャンマー語・標準＞
+- [x] Pashto_Pickup.html ＜パシュトゥ語（アフガニスタン）＞
+- [x] Assamese_Pickup.html ＜アッサム語＞
+- [x] Sanskrit_Pickup.html ＜サンスクリット語＞
+- [x] Bengali_Pickup.html ＜ベンガル語＞
+- [x] kmer_Cambodian.html ＜クメール・カンボジア語＞
+- [x] Tamil_Pickup.html ＜タミル語＞
 - [ ] Sinhala_EM_Pickup.html ＜シンハラ語＞
 - [ ] Napali_Pickup.html ＜ネパール語＞
 - [ ] Hindi_Pickup.html ＜ヒンディー語＞
@@ -41,7 +36,7 @@
 - [ ] Suomi_Pickup.html ＜スオミ語＞
 - [ ] French_Pho_Pickup.html ＜フランス語＆IPA＞
 
-## 後回しのやつ
+## 後回し/対応なし
 
 - [ ] korean_Pickup.html ＜韓国語＞
 - [ ] korean_PickupV1.html ＜韓国語PRO＞
